ade31ed1a9a925155f55d155cf820c13e66e420e
Arthur Léothaud <arthur@leothaud.eu>
2014-06-14 00:26:16 +0200

    refactor setup-dotfiles.sh
    Remove unnecessary operations and echos.
    
    Add tests whether former files exist.
    
    Variables instead of hard references to various dirs.

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -6,28 +6,28 @@
 
 ########## Variables
 
-DIR=~/dotfiles # dotfiles directory
-OLD_DIR=~/dotfiles_old # old dotfiles backup directory
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+DIR=$SCRIPTPATH
+TARGET_DIR=$HOME # destination directory
+OLD_DIR=$TARGET_DIR/dotfiles_old # old dotfiles backup directory
 FILES="bash_aliases bashrc inputrc liquidpromptrc lynxrc muttrc profile tmux.conf vimrc" # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $OLD_DIR for backup of any existing dotfiles in ~"
+echo "Creating backup dir $OLD_DIR"
 mkdir -p $OLD_DIR
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $DIR directory"
-cd $DIR
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $FILES; do
-    echo "Moving any existing dotfiles from ~ to $OLD_DIR"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $DIR/$file ~/.$file
+for filename in $FILES; do
+	file=$TARGET_DIR/.$filename
+	if [ -e $file ]; then
+		echo "Moving $file to $OLD_DIR."
+		mv $file $OLD_DIR
+	fi
+
+	echo "Creating $file@."
+    ln -s $DIR/$filename $file
 done
 
 # EOF
