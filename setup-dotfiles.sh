81fb0cc743122c9935e88d4f0d31a413445fdf01
Arthur Léothaud <duncan@ns301170.ip-91-121-73.eu>
2014-09-12 13:35:24 +0200

    Refacto + add timestamp in old_dir
    Timestamp yyy-mm-dd appended at the end of backup dir name
    
    miscellaneous refactoring

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
-############################
-# This script creates symlinks from the home directory to any desired
-# dotfiles in ~/dotfiles
-############################
+#####################################################################
+# This script creates symlinks in the $HOME directory of all dotfiles
+# present in the script’s directory. Dotfiles already present in $HOME
+# are backed up in a timestamped directory.
+#####################################################################
 
-########## Variables
+TIME_STAMP=`date +%Y-%m-%d` # date in format YYYY-MM-DD
+DOT_FILES_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+DOT_FILES="bash_aliases bashrc inputrc liquidpromptrc lynxrc muttrc profile tmux.conf vimrc"
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-DIR=$SCRIPTPATH
 TARGET_DIR=$HOME # destination directory
-OLD_DIR=$TARGET_DIR/dotfiles_old # old dotfiles backup directory
-FILES="bash_aliases bashrc inputrc liquidpromptrc lynxrc muttrc profile tmux.conf vimrc" # list of files/folders to symlink in homedir
+OLD_DIR=$TARGET_DIR/dotfiles_$TIME_STAMP # old dotfiles backup directory
 
-##########
-
-# create dotfiles_old in homedir
-echo "Creating backup dir $OLD_DIR"
+echo "Creating backup directory: $OLD_DIR"
 mkdir -p $OLD_DIR
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for filename in $FILES; do
+# Backup existing dotfiles in TARGET_DIR to OLD_DIR
+for filename in $DOT_FILES; do
 	file=$TARGET_DIR/.$filename
 	if [ -e $file ]; then
-		echo "Moving $file to $OLD_DIR."
+		echo "Moving $file to $OLD_DIR"
 		mv $file $OLD_DIR
 	fi
 
-	echo "Creating $file@."
-    ln -s $DIR/$filename $file
+	# then create symlinks
+	echo "Creating $file@"
+    ln -s $DOT_FILES_DIR/$filename $file
 done
 
 # EOF
