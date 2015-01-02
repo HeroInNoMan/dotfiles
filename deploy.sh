f3a857206a8c62652067897cd7b94c1177a9f791
Arthur Léothaud <arthur@leothaud.eu>
2015-01-02 13:47:53 +0100

    Add deployment of .localrc + minor refactorings

--- deploy.sh
+++ deploy.sh
@@ -15,45 +15,30 @@ TARGET_DIR=$HOME # destination directory
 BACKUP_DIR=$TARGET_DIR/dotfiles_$TIME_STAMP # old dotfiles backup directory
 
 deploy () {
-	source_file=$1
-	target_file=$2
+	source=$1
+	link=$2
 	backup_place=$3
 
-	# if target dir for config file does not exist, exit
-	if [ ! -d $(dirname $target_file) ]; then
-		echo "$(basename $target_file) : no directory $(dirname $target_file), aborting."
+	if [ ! -d $(dirname $link) ]; then
+		echo "$(basename $link) : no directory $(dirname $link), aborting."
 		return
 	fi
 
-	if [ -L $target_file ]; then
-		TARGET=`readlink $target_file`
-		if [ $TARGET == $source_file ]; then
-			echo "$target_file is already a link to $TARGET"
+	if [ -L $link ]; then
+		target=`readlink $link`
+		if [ $target == $source ]; then
+			echo "$link is already a link to $target"
 			return
 		fi
-		echo "Deleting $target_file which is a link to $TARGET"
-		rm $target_file
-	elif [ -e $target_file ]; then
-		echo "Moving $target_file to $backup_place"
-		mv $target_file $backup_place
+		echo "Deleting $link which is a link to $target"
+		rm $link
+	elif [ -e $link ]; then
+		echo "Moving $link to $backup_place"
+		mv $link $backup_place
 	fi
-	# then create symlinks
-	echo "Creating $target_file@"
-	ln -s $source_file $target_file
-}
 
-deploy_dot_file () {
-	source_file=$1
-	stripped_source_file=`echo basename $source_file | cut -d'_' -f 2-`
-	target_file=$TARGET_DIR/.$stripped_source_file
-	deploy $source_file $target_file $BACKUP_DIR
-}
-
-deploy_config_file () {
-	source_file="${DOT_FILES_DIR}/$1"
-	target_file="$HOME/$2"
-	backup_file="${target_file}_$TIME_STAMP"
-	deploy $source_file $target_file $backup_file
+	echo "Creating $link@"
+	ln -s $source $link
 }
 
 #####################################################################
@@ -61,8 +46,10 @@ deploy_config_file () {
 mkdir -p $BACKUP_DIR
 
 # DOTFILES
-for filename in $(ls -1 $DOT_FILES_DIR/dot_*); do
-	deploy_dot_file $filename
+for file in $(ls -1 $DOT_FILES_DIR/dot_*); do
+	root_name=`echo basename $file | cut -d'_' -f 2-`
+	target=$TARGET_DIR/.$root_name
+	deploy $file $target $BACKUP_DIR
 done
 
 # if no backup was made, delete backup dir
@@ -71,9 +58,12 @@ rmdir --ignore-fail-on-non-empty $BACKUP_DIR
 # CONF FILES
 
 # irssi
-deploy_config_file irssi_config .irssi/config
+deploy "$DOT_FILES_DIR/irssi_config" "$TARGET_DIR/.irssi/config" "${TARGET_DIR}/.irssi/config_$TIME_STAMP"
 
 # lubuntu-rc.xml
-deploy_config_file lubuntu-rc.xml .config/openbox/lubuntu-rc.xml
+deploy "$DOT_FILES_DIR/lubuntu-rc.xml" "$TARGET_DIR/.config/openbox/lubuntu-rc.xml" ".config/openbox/lubuntu-rc.xml_$TIME_STAMP"
+
+# .localrc : copy if not present (no symlink)
+	cp -n $DOT_FILES_DIR/dot_localrc $TARGET_DIR/.localrc
 
 # EOF
