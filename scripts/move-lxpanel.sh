3e10cc3c140f53d04a4f6d35f9857c4cbe09f81e
Arthur Léothaud <arthur@leothaud.eu>
2019-11-23 21:44:10 +0100

    add script to move lxpanel from one display to another

---
+++ scripts/move-lxpanel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+PANEL_CONFIG_FILE=$HOME/.config/lxpanel/default/panels/panel
+
+# find number of monitors
+NB_DISPLAYS=$(xrandr --query | grep --count ' connected')
+
+# get current lxpanel monitor number
+CURRENT_MONITOR_NUMBER=$(grep "monitor=[0-9]" $PANEL_CONFIG_FILE | cut -d '=' -f2)
+
+# change
+NEW_MONITOR=$((($CURRENT_MONITOR_NUMBER + 1) % $NB_DISPLAYS))
+sed -i "s/monitor=$CURRENT_MONITOR_NUMBER/monitor=$NEW_MONITOR/" $PANEL_CONFIG_FILE
+echo "Switching panel from display #$CURRENT_MONITOR_NUMBER to #$NEW_MONITOR ($NB_DISPLAYS displays found in total)"
+
+# apply changes
+lxpanelctl restart
