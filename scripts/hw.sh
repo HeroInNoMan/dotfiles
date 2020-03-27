1c28f4460e9520c5628b6c7bc2bbe8e1ea105be4
Arthur Léothaud <hero.in.no.man@gmail.com>
2020-03-27 12:28:07 +0100

    reformat

--- scripts/hw.sh
+++ scripts/hw.sh
@@ -19,33 +19,33 @@ MUTE_STATE=$(pacmd dump | grep --perl-regexp "^set-sink-mute $SINK_NAME\s+" | pe
 BRIGHTNESS_FILE="/sys/class/backlight/intel_backlight/brightness"
 
 display-current-state () {
-    echo "toto"
+	echo "toto"
 }
 
 notif () {
-    notify-send "$1" --expire-time=500 --icon="$2" --urgency=NORMAL
+	notify-send "$1" --expire-time=500 --icon="$2" --urgency=NORMAL
 }
 
 light_down () {
-    xbacklight -dec 30
-    [ $? != 0 ] && echo $(($(cat $BRIGHTNESS_FILE) - 100)) > $BRIGHTNESS_FILE
-    # notif "↓" "$BRIGHTNESS_IMG"
+	xbacklight -dec 30
+	[ $? != 0 ] && echo $(($(cat $BRIGHTNESS_FILE) - 100)) > $BRIGHTNESS_FILE
+	# notif "↓" "$BRIGHTNESS_IMG"
 }
 
 light_up () {
-    xbacklight -inc 30
-    [ $? != 0 ] && echo $(($(cat $BRIGHTNESS_FILE) + 100)) > $BRIGHTNESS_FILE
-    # notif "↑" "$BRIGHTNESS_IMG"
+	xbacklight -inc 30
+	[ $? != 0 ] && echo $(($(cat $BRIGHTNESS_FILE) + 100)) > $BRIGHTNESS_FILE
+	# notif "↑" "$BRIGHTNESS_IMG"
 }
 
 sound_down () {
 	for ctrl in $(amixer scontrols | grep 'Simple mixer control' | cut -d\' -f 2 | sort | uniq); do
-		amixer -q sset "$ctrl" 3%- unmute
+		amixer -q sset $ctrl ${SOUND_CHANGE_STEP}%- unmute
 	done
-    for sink in $SINKS; do
-        pactl set-sink-volume "$sink" -5%
-    done
-    notif "Volume down" "$SOUND_DOWN_IMG"
+	for sink in $SINKS; do
+		pactl set-sink-volume $sink -${SOUND_CHANGE_STEP}%
+	done
+	notif "Volume down" "$SOUND_DOWN_IMG"
 }
 
 sound_up () {
@@ -59,67 +59,67 @@ sound_up () {
 }
 
 toggle_mute () {
-    NOTIF_TEXT=$([[ $MUTE_STATE == 'yes' ]] && echo 'ON' || echo 'OFF')
-    NOTIF_IMG=$([[ $MUTE_STATE == 'yes' ]] && echo "$UNMUTE_IMG" || echo "$MUTE_IMG")
-    SINK_STATE=$([[ $MUTE_STATE == 'yes' ]] && echo '0' || echo '1')
-    for sink in $SINKS; do
-        pactl set-sink-mute "$sink" "$SINK_STATE"
-        [ $? -eq 0 ] && notif "$NOTIF_TEXT" "$NOTIF_IMG"
-    done
+	NOTIF_TEXT=$([[ $MUTE_STATE == 'yes' ]] && echo 'ON' || echo 'OFF')
+	NOTIF_IMG=$([[ $MUTE_STATE == 'yes' ]] && echo "$UNMUTE_IMG" || echo "$MUTE_IMG")
+	SINK_STATE=$([[ $MUTE_STATE == 'yes' ]] && echo '0' || echo '1')
+	for sink in $SINKS; do
+		pactl set-sink-mute "$sink" "$SINK_STATE"
+		[ $? -eq 0 ] && notif "$NOTIF_TEXT" "$NOTIF_IMG"
+	done
 }
 
 toggle_trackpad () {
-  TRACKPAD_IMG="$HOME/.config/img/mouse_warning.png"
+	TRACKPAD_IMG="$HOME/.config/img/mouse_warning.png"
 
-  MAX_TAP_TIME=$(synclient -l | grep --regexp='MaxTapTime' | cut --delimiter='=' --fields=2 | tr --delete '[:blank:]')
+	MAX_TAP_TIME=$(synclient -l | grep --regexp='MaxTapTime' | cut --delimiter='=' --fields=2 | tr --delete '[:blank:]')
 
-  [ -z "$MAX_TAP_TIME" ] && exit 1
+	[ -z "$MAX_TAP_TIME" ] && exit 1
 
-  if [ "$MAX_TAP_TIME" -gt 0 ]; then
-    synclient MaxTapTime=0
-    notif "OFF" "$TRACKPAD_IMG"
-  else
-    synclient MaxTapTime=100
-    notif "ON" "$TRACKPAD_IMG"
-  fi
+	if [ "$MAX_TAP_TIME" -gt 0 ]; then
+		synclient MaxTapTime=0
+		notif "OFF" "$TRACKPAD_IMG"
+	else
+		synclient MaxTapTime=100
+		notif "ON" "$TRACKPAD_IMG"
+	fi
 
-  # enable vertical & horizontal scrolling on edge or with two fingers
-  synclient VertEdgeScroll=1
-  synclient VertTwoFingerScroll=1
-  synclient HorizEdgeScroll=1
-  synclient HorizTwoFingerScroll=1
+	# enable vertical & horizontal scrolling on edge or with two fingers
+	synclient VertEdgeScroll=1
+	synclient VertTwoFingerScroll=1
+	synclient HorizEdgeScroll=1
+	synclient HorizTwoFingerScroll=1
 }
 
 usage () {
-    echo "Usage:"
-    echo "$0 light-up | light-down"
-    echo "$0 sound-toggle | sound-up | sound-down"
-    echo "$0 display | display-previous | display-dual-screen-copy | display-built-in-only | display-extend-left | display-extend-right | display-extend-up | display-extend-down | display-external-only"
-    echo "$0 trackpad"
+	echo "Usage:"
+	echo "$0 light-up | light-down"
+	echo "$0 sound-toggle | sound-up | sound-down"
+	echo "$0 display | display-previous | display-dual-screen-copy | display-built-in-only | display-extend-left | display-extend-right | display-extend-up | display-extend-down | display-external-only"
+	echo "$0 trackpad"
 }
 
 if [ $# == 1 ]; then
-    case $1 in
-        "display") manage-displays.sh ;;
-        "display-previous") manage-displays.sh previous ;;
-        "display-dual-screen-copy") manage-displays.sh dual-screen-copy ;;
-        "display-built-in-only") manage-displays.sh built-in-only ;;
-        "display-extend-left") manage-displays.sh extend-left ;;
-        "display-extend-right") manage-displays.sh extend-right ;;
-        "display-extend-up") manage-displays.sh extend-up ;;
-        "display-extend-down") manage-displays.sh extend-down ;;
-        "display-external-only") manage-displays.sh external-only ;;
-        "light-down") light_down ;;
-        "light-up") light_up ;;
-        "sound-down") sound_down ;;
-        "sound-up") sound_up ;;
-        "sound-toggle") toggle_mute ;;
-        "trackpad") toggle_trackpad ;;
-        *) usage ;;
-    esac
+	case $1 in
+		"display") manage-displays.sh ;;
+		"display-previous") manage-displays.sh previous ;;
+		"display-dual-screen-copy") manage-displays.sh dual-screen-copy ;;
+		"display-built-in-only") manage-displays.sh built-in-only ;;
+		"display-extend-left") manage-displays.sh extend-left ;;
+		"display-extend-right") manage-displays.sh extend-right ;;
+		"display-extend-up") manage-displays.sh extend-up ;;
+		"display-extend-down") manage-displays.sh extend-down ;;
+		"display-external-only") manage-displays.sh external-only ;;
+		"light-down") light_down ;;
+		"light-up") light_up ;;
+		"sound-down") sound_down ;;
+		"sound-up") sound_up ;;
+		"sound-toggle") toggle_mute ;;
+		"trackpad") toggle_trackpad ;;
+		*) usage ;;
+	esac
 else
-    usage
-    exit 1
+	usage
+	exit 1
 fi
 
 # EOF
