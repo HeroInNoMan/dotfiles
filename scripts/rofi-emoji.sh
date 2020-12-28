65b3d0a704468578e3e616b514a3249d43cebf8b
Arthur Léothaud <arthur@leothaud.eu>
2020-12-28 04:37:57 +0100

    add rofi script to insert emoji.
    Depends on https://github.com/fdw/rofimoji

---
+++ scripts/rofi-emoji.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+rofimoji --action clipboard \
+         --skin-tone neutral \
+         --rofi-args="-location 6 \
+                      -opacity 10 \
+                      -lines 40 \
+                      -width 95 \
+                      -columns 4 \
+                      -bg '#262626' \
+                      -bgalt '#212121' \
+                      -fg '#ffffff' \
+                      -hlbg '#d64937' \
+                      -hlfg '#ffffff' "
+# EOF
