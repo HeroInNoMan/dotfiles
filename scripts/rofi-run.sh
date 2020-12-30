2ebbbf9e54b61b52d52948f3d32e4956f5471680
Arthur Léothaud <arthur@leothaud.eu>
2020-12-30 18:02:10 +0100

    format rofi-run.sh

--- scripts/rofi-run.sh
+++ scripts/rofi-run.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 0 ]; then
-    modi="$1";
+  modi="$1";
 else
-    modi="run";
+  modi="run";
 fi
-rofi -show $modi -bg '#262626' -bgalt '#212121' -fg '#ffffff' -hlbg '#d64937' -hlfg '#ffffff' -font monospace\ 12 -opacity "90" -location 6 -lines 30 -terminal lxterminal
+rofi \
+  -show $modi \
+  -bg '#262626' \
+  -bgalt '#212121' \
+  -fg '#ffffff' \
+  -hlbg '#d64937' \
+  -hlfg '#ffffff' \
+  -font monospace\ 12 \
+  -opacity "90" \
+  -location 6 \
+  -lines 30 \
+  -terminal lxterminal
 
 # EOF
