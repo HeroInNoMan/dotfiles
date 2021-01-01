f1a518dc8d1adf05ec72323bace58adff6d7cd32
Arthur Léothaud <arthur@leothaud.eu>
2021-01-02 00:51:40 +0100

    improve switch_kb script with function calls

--- scripts/switch_kb.sh
+++ scripts/switch_kb.sh
@@ -12,45 +12,54 @@ notif-change-layout () {
   notify-send "$LAYOUT_NAME" --expire-time=1000 --icon=$KBD_IMG --category="Layout"
 }
 
-case $LAYOUT in
-  "fr(bepo)")
-    setxkbmap fr -option
-    echo "fr" > $KBD_FILE
-    NOTIF="AZERTY"
-    ;;
-  *)
-    setxkbmap fr bepo -option ctrl:nocaps compose:prsc
-    echo "fr(bepo)" > $KBD_FILE
-    NOTIF="BÉPO"
-    ;;
-esac
+setup_typematrix () {
+  # TypeMatrix → toujours en bépo sans options
+  for id in $(xinput list | grep -i "typematrix" | cut -d= -f2 | cut -f1); do
+    setxkbmap -layout 'fr(bepo)' -device "$id" -option 2> /dev/null
+    TYPEMATRIX="true"
+  done
+  if [ -n "$TYPEMATRIX" ] && [ ! "BÉPO" == "$NOTIF" ];
+  then
+    NOTIF="TM:BÉPO, $NOTIF"
+  fi
+}
 
-# TypeMatrix → toujours en bépo sans options
-for id in $(xinput list | grep -i "typematrix" | cut -d= -f2 | cut -f1); do
-  setxkbmap -layout 'fr(bepo)' -device "$id" -option 2> /dev/null
-  TYPEMATRIX="true"
-done
-if [ -n "$TYPEMATRIX" ] && [ ! "BÉPO" == "$NOTIF" ];
-then
-  NOTIF="TM:BÉPO, $NOTIF"
-fi
+setup_ergodox () {
+  # Ergodox → toujours en bépo sans options
+  for id in $(xinput list | grep -i "ergodox" | cut -d= -f2 | cut -f1); do
+    setxkbmap -layout 'fr(bepo)' -device "$id" -option 2> /dev/null
+    ERGODOX="true"
+  done
+  if [ -n "$ERGODOX" ] && [ ! "BÉPO" == "$NOTIF" ];
+  then
+    NOTIF="EZ:BÉPO, $NOTIF"
+  fi
+}
 
+setup_default () {
+  case $LAYOUT in
+    "fr(bepo)")
+      setxkbmap fr -option
+      echo "fr" > $KBD_FILE
+      NOTIF="AZERTY"
+      ;;
+    *)
+      setxkbmap fr bepo -option ctrl:nocaps compose:prsc
+      echo "fr(bepo)" > $KBD_FILE
+      NOTIF="BÉPO"
+      ;;
+  esac
+}
 
+main () {
+  setup_default
+  setup_typematrix
+  setup_ergodox
+  notif-change-layout "$NOTIF"
+  [[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
+}
 
-# Ergodox → toujours en bépo sans options
-for id in $(xinput list | grep -i "ergodox" | cut -d= -f2 | cut -f1); do
-  setxkbmap -layout 'fr(bepo)' -device "$id" -option 2> /dev/null
-  ERGODOX="true"
-done
-if [ -n "$ERGODOX" ] && [ ! "BÉPO" == "$NOTIF" ];
-then
-  NOTIF="EZ:BÉPO, $NOTIF"
-fi
-
-notif-change-layout "$NOTIF"
-
-[[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
-
+main
 # llkk
 # llk
 
