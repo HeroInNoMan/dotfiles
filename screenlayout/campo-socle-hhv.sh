0cf834b2f7f55561cc0667d6c1dc86c0710b7a49
Arthur Léothaud <hero.in.no.man@gmail.com>
2021-01-14 19:36:26 +0100

    add screenlayout configs

---
+++ screenlayout/campo-socle-hhv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-2 --mode 1920x1080 --pos 3200x0 --rotate right --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 1280x384 --rotate normal --output eDP-1 --primary --mode 1280x720 --pos 0x744 --rotate normal --output DP-2 --off
