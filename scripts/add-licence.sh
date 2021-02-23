1fd4df30bb2a102c207489d2459a35010788f93a
Arthur Léothaud <hero.in.no.man@gmail.com>
2021-02-23 19:41:37 +0100

    update licence script

--- scripts/add-licence.sh
+++ scripts/add-licence.sh
@@ -5,22 +5,21 @@
 ###############################################################################
 
 # goto project root ###########################################################
-old_dir=$(pwd)
+old_dir=$PWD
 ROOT_DIR=$(git rev-parse --show-toplevel)
 cd $ROOT_DIR
 # write license file ##########################################################
 cat >LICENSE <<EOL
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE
-                    Version 3.1, July 2019
+              DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE
+                         Version 3.1, July 2019
+                     https://ph.dtf.wtf/u/wtfplv31
 
- by Sam Hocevar <sam@hocevar.net>
-    theiostream <matoe@matoe.co.cc>
-    dtf         <wtfpl@dtf.wtf>
+by Arthur Léothaud <arthur@leothaud.eu>
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+              DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE
+    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
-  0. You just DO WHAT THE FUCK YOU WANT TO.
+0. You just DO WHAT THE FUCK YOU WANT TO.
 EOL
 
 # go back to old_dir ##########################################################
