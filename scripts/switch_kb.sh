d007d8ae14b5f2dd2eb75f7233df828a9ee2d3ef
Arthur Léothaud <arthur@leothaud.eu>
2021-08-13 11:00:28 +0200

    fix all flycheck errors in switch_kb.sh

--- scripts/switch_kb.sh
+++ scripts/switch_kb.sh
@@ -6,20 +6,20 @@ BEPO_VERSION="bepo_afnor"
 
 KBD_IMG="$HOME/.config/img/keyboard.png"
 KBD_FILE="$HOME/.kbd-switch"
-true >> $KBD_FILE
-LAYOUT=$(head -1 $KBD_FILE)
+true >> "$KBD_FILE"
+LAYOUT=$(head -1 "$KBD_FILE")
 
 notif-change-layout () {
   LAYOUT_NAME=$1
   NEW_LAYOUT=$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')
   echo "$CURRENT_LAYOUT → $NEW_LAYOUT ($LAYOUT_NAME)"
-  notify-send "$LAYOUT_NAME" --expire-time=1000 --icon=$KBD_IMG --category="Layout"
+  notify-send "$LAYOUT_NAME" --expire-time=1000 --icon="$KBD_IMG" --category="Layout"
 }
 
 setup_typematrix () {
   # TypeMatrix → toujours en bépo sans options
   for id in $(xinput list | grep -i "typematrix" | cut -d= -f2 | cut -f1); do
-    setxkbmap fr bepo_afnor -device "$id" -option 2> /dev/null
+    setxkbmap fr $BEPO_VERSION -device "$id" -option 2> /dev/null
     TYPEMATRIX="true"
   done
   if [ -n "$TYPEMATRIX" ] && [ ! "BÉPO" == "$NOTIF" ];
@@ -31,7 +31,7 @@ setup_typematrix () {
 setup_ergodox () {
   # Ergodox → toujours en bépo sans options
   for id in $(xinput list | grep -i "ergodox" | cut -d= -f2 | cut -f1); do
-    setxkbmap fr bepo_afnor -device "$id" -option 2> /dev/null
+    setxkbmap fr $BEPO_VERSION -device "$id" -option 2> /dev/null
     ERGODOX="true"
   done
   if [ -n "$ERGODOX" ] && [ ! "BÉPO" == "$NOTIF" ];
@@ -44,12 +44,12 @@ setup_default () {
   case $LAYOUT in
     "fr(bepo)")
       setxkbmap fr -option
-      echo "fr" > $KBD_FILE
+      echo "fr" > "$KBD_FILE"
       NOTIF="AZERTY"
       ;;
     *)
-      setxkbmap fr bepo_afnor -option ctrl:nocaps compose:prsc
-      echo "fr(bepo)" > $KBD_FILE
+      setxkbmap fr $BEPO_VERSION -option ctrl:nocaps compose:prsc
+      echo "fr(bepo)" > "$KBD_FILE"
       NOTIF="BÉPO"
       ;;
   esac
