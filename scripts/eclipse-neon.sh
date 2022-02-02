3cc94112038eff3a5863ea7b3a067375e31f8d28
Arthur Léothaud <hero.in.no.man@gmail.com>
2022-02-02 15:15:15 +0100

    change eclipse launcher(s)

--- scripts/eclipse-neon.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export SWT_GTK3=0
-export GTK2_RC_FILES=~/.gtkrc-2.0-gnome-color-chooser
-
-$HOME/outils/eclipse/jee-neon/eclipse/eclipse
-
-# EOF
---
+++ scripts/eclipse.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+WORKSPACE="$1"
+[[ $WORKSPACE ]] || WORKSPACE="$HOME/outils/eclipse/workspaces/enercoop-coopener-v5"
+# $HOME/outils/eclipse/workspaces/xxx
+export ECLIPSE_BINARY="$HOME/outils/eclipse/jee-2022-06/eclipse/eclipse"
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+
+# export SWT_GTK3=0
+# export GTK2_RC_FILES=~/.gtkrc-2.0-gnome-color-chooser
+
+source ${SDKMAN_DIR}/bin/sdkman-init.sh
+sdk use java $JAVA_17_VERSION
+
+$ECLIPSE_BINARY
+
+# EOF
