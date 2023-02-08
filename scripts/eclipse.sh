2bdd222130bf02ab0e478d8f5087cd7a55d3b3bc
Arthur Léothaud <hero.in.no.man@gmail.com>
2023-02-08 19:33:54 +0100

    update eclipse launcher

--- scripts/eclipse.sh
+++ scripts/eclipse.sh
@@ -3,7 +3,7 @@
 WORKSPACE="$1"
 [[ $WORKSPACE ]] || WORKSPACE="$HOME/outils/eclipse/workspaces/enercoop-coopener-v5"
 # $HOME/outils/eclipse/workspaces/xxx
-export ECLIPSE_BINARY="$HOME/outils/eclipse/jee-2022-06/eclipse/eclipse"
+export ECLIPSE_BINARY="$HOME/outils/eclipse/jee-2022-12/eclipse/eclipse"
 export GTK_IM_MODULE=ibus
 export QT_IM_MODULE=ibus
 
