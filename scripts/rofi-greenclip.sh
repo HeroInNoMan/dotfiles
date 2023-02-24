6ed88e789b3f7d5f31553bd1b1ea69f2e6a5d544
Arthur Léothaud <arthur@leothaud.eu>
2023-02-24 15:38:09 +0100

    add greenclip

---
+++ scripts/rofi-greenclip.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if ! hash greenclip 2> /dev/null; then
+  echo "Installing greenclip…"
+  cd ~/bin/ || return
+  wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip
+  chmod a+x greenclip
+  cd - || return
+fi
+
+if [ $(pgrep -f "greenclip daemon" | wc -l) -lt 1 ]; then
+  echo "Starting greenclip daemon…"
+  greenclip daemon &
+fi
+
+rofi -modi "clipboard:greenclip print" -show clipboard -theme "repos/dotfiles/rofi/run.rasi" -run-command '{cmd}'
+
+# EOF
