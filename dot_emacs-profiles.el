010d722311cc0df75f77e4abf416cf6071c4812e
Arthur Léothaud <hero.in.no.man@gmail.com>
2024-01-31 15:24:09 +0100

    update emacs meta conf

--- dot_emacs-profiles.el
+++ dot_emacs-profiles.el
@@ -1,4 +1,5 @@
 (("default"   . ((user-emacs-directory . "~/repos/my-emacs")      (server-name . "gnu")))
+ ("gnu"       . ((user-emacs-directory . "~/repos/my-emacs")      (server-name . "gnu")))
  ("doom"      . ((user-emacs-directory . "~/repos/doom-emacs")    (server-name . "doom")      (env . (("DOOMDIR" . "~/.doom.d")))))
  ("spacemacs" . ((user-emacs-directory . "~/repos/spacemacs")     (server-name . "spacemacs") (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ("centaur"   . ((user-emacs-directory . "~/repos/centaur-emacs") (server-name . "centaur")))
