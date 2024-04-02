f1e8fe163a908411758f559b7e5bb63c2d426119
Arthur Léothaud <hero.in.no.man@gmail.com>
2024-04-02 16:30:44 +0200

    [WIP] update emacsclient-todo.sh

--- scripts/emacsclient-todo.sh
+++ scripts/emacsclient-todo.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-export GTK_IM_MODULE=xim
-export QT_IM_MODULE=xim
-
-emacsclient -s gnu -c -a '' -e "(progn (org-capture nil \"w\")
-(delete-other-windows))"
+emacsclient.sh gnu "(progn \
+(setq external-capture t) \
+ (org-capture nil \"w\") \
+ (delete-other-windows))"
 
 # EOF
