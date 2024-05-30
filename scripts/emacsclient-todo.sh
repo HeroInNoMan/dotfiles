286a70195247fe485c0c291f23897fbe5196acab
Arthur Léothaud <hero.in.no.man@gmail.com>
2024-05-30 11:04:02 +0200

    fix emacsclient-todo.sh

--- scripts/emacsclient-todo.sh
+++ scripts/emacsclient-todo.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-emacsclient.sh gnu "(progn \
-(setq external-capture t) \
- (org-capture nil \"w\") \
- (delete-other-windows))"
+SERVER_NAME=$(head -1 "$HOME/.emacs-profile" | tr -d '\n')
+emacsclient.sh $SERVER_NAME "(progn \
+               (setq newframe-capture t)
+               (org-capture nil \"w\") \
+               (delete-other-windows))"
 
 # EOF
