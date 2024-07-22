d32417cf21a52d038d8664f75c2d7bf847d08d81
Arthur Léothaud <hero.in.no.man@gmail.com>
2024-07-22 21:59:12 +0200

    improve emacs-related functions

--- scripts/emacsclient-todo.sh
+++ scripts/emacsclient-todo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+# LETTER=t # t for personal TODO
+LETTER=w # w for work TODO
 SERVER_NAME=$(head -1 "$HOME/.emacs-profile" | tr -d '\n')
 emacsclient.sh $SERVER_NAME "(progn \
-               (setq newframe-capture t)
-               (org-capture nil \"w\") \
+               (setq newframe-capture t) \
+               (org-capture nil \""$LETTER"\") \
                (delete-other-windows))"
-
 # EOF
