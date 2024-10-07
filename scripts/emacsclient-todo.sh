86a8859f85f970e035f2329639e8cc6cf56c67d7
Arthur Léothaud <hero.in.no.man@gmail.com>
2024-10-07 17:29:21 +0200

    [emacs] use “prot” version of disposable popups

--- scripts/emacsclient-todo.sh
+++ scripts/emacsclient-todo.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-# LETTER=t # t for personal TODO
-LETTER=w # w for work TODO
-SERVER_NAME=$(head -1 "$HOME/.emacs-profile" | tr -d '\n')
-emacsclient.sh $SERVER_NAME "(progn \
-               (setq newframe-capture t) \
-               (org-capture nil \""$LETTER"\") \
-               (delete-other-windows))"
-# EOF
+emacsclient -r -s "gnu" -e '(prot-window-popup-org-capture)'
